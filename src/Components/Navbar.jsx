4ea00abbdac3ea694521706307fadbe6326475cc
Your Name <your-email@example.com>
2026-01-23 18:02:37 +0530

    dashboard glitch fixed

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -2,8 +2,10 @@ import axios from "axios"
 import Base_url from "../Constants/Base_url";
 import { useNavigate } from "react-router-dom";
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
 const Navbar= ()=>{
+  const [open,setOpen]=useState(false)
     const navigate=useNavigate();
     const handleLogout=async()=>{
         try{
@@ -16,26 +18,64 @@ const Navbar= ()=>{
     }
 
   return (
-    <div className="
-      w-full
-      bg-[#020617]
-      border-b border-slate-800
-      shadow-lg
-    ">
-      <div className="flex justify-between items-center px-10 py-5">
-        <h1 className="text-2xl font-bold text-white tracking-wide">
+    <div className="w-full bg-[#020617] border-b border-slate-800 relative z-50">
+      <div className="flex items-center justify-between h-16 px-8">
+
+        {/* Logo */}
+        <h1 className="text-xl font-bold text-white">
           Vault<span className="text-blue-500">Pay</span>
         </h1>
 
-        <button
-          onClick={handleLogout}
-          className="btn btn-sm btn-outline text-white border-slate-500 hover:bg-white hover:text-black"
+        {/* Profile */}
+        <div className="relative">
+          <div onClick={()=>setOpen(!open)}
+           className="avatar">
+        <div
+          className="
+            w-9 h-9 rounded-full
+            ring ring-blue-500
+            ring-offset-2 ring-offset-[#020617]
+            bg-[#020617]
+            overflow-hidden
+            shadow-md
+          "
         >
-          Logout
-        </button>
+          <img
+            src="https://img.freepik.com/premium-vector/avatar-profile-picture-icon-blue-background-flat-design-style-resources-graphic-element-design_991720-653.jpg"
+            alt="user avatar"
+            className="w-full h-full object-cover"
+          />
+        </div>
+          </div>
+
+          {/* Dropdown */}
+          {open && (
+            <div className="absolute right-0 mt-3 w-40 bg-[#0f172a] rounded-lg shadow-lg overflow-hidden">
+              <Link
+                to="/"
+                className="block px-4 py-2 text-white hover:bg-[#1e2a45]"
+                onClick={() => setOpen(false)}
+              >
+                Dashboard
+              </Link>
+
+              <button
+                onClick={handleLogout}
+                className="block w-full text-left px-4 py-2 text-white hover:bg-[#1e2a45]"
+              >
+                Logout
+              </button>
+            </div>
+          )}
+        </div>
+
       </div>
     </div>
   );
 }
 
 export default Navbar;
+
+
+
+
